dae08369ac84fe2409ea9397e75644d5e296a4df
Claire Cho <claire.eunbee@gmail.com>
2019-04-21 09:16:41 -0400

    testing

--- Create a Drink/TableViewController.swift
+++ Create a Drink/TableViewController.swift
@@ -20,10 +20,12 @@ struct cellData {
 }
 
 class TableViewController: UITableViewController {
-    
+
+    @IBOutlet var searchBar: UISearchBar!
     var arrayOfCellData = [cellData]()
     
     override func viewDidLoad() {
+        searchBar.backgroundImage = UIImage()
         arrayOfCellData = [cellData(cell : 1, image : #imageLiteral(resourceName: "apple"), label : "Apple", color : #colorLiteral(red: 0.970673382, green: 0.7970152738, blue: 0.8244020901, alpha: 1), type : "apple", ingredientColor : #colorLiteral(red: 1, green: 0.3113977491, blue: 0.2768607729, alpha: 1)),
                            cellData(cell : 1, image : #imageLiteral(resourceName: "banana"), label : "Banana", color : #colorLiteral(red: 1, green: 0.8210952197, blue: 0.8493094643, alpha: 1), type : "banana", ingredientColor : #colorLiteral(red: 0.9764705896, green: 0.9443702147, blue: 0.3245594026, alpha: 1)),
                            cellData(cell : 1, image : #imageLiteral(resourceName: "cherry"), label : "Cherry", color : #colorLiteral(red: 0.970673382, green: 0.7970152738, blue: 0.8244020901, alpha: 1), type : "cherry", ingredientColor : #colorLiteral(red: 0.521568656, green: 0.1098039225, blue: 0.05098039284, alpha: 1)),
